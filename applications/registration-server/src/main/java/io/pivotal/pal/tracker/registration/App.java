bbaf336c4614205502815b557be7f4963530a4b2
adamos486 <adamos486@gmail.com>
2018-10-29 13:15:44 -0700

    Updating each App to use EurekaClient

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -1,22 +1,17 @@
 package io.pivotal.pal.tracker.registration;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.TimeZone;
-
-
-@SpringBootApplication
-@ComponentScan({
-    "io.pivotal.pal.tracker.accounts",
-    "io.pivotal.pal.tracker.restsupport",
-    "io.pivotal.pal.tracker.projects",
-    "io.pivotal.pal.tracker.users"
-})
-public class App {
-    public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        SpringApplication.run(App.class, args);
-    }
+@SpringBootApplication @ComponentScan({
+    "io.pivotal.pal.tracker.accounts", "io.pivotal.pal.tracker.restsupport",
+    "io.pivotal.pal.tracker.projects", "io.pivotal.pal.tracker.users"
+}) @EnableEurekaClient public class App {
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    SpringApplication.run(App.class, args);
+  }
 }
--- applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
+++ applications/timesheets-server/src/main/java/io/pivotal/pal/tracker/timesheets/App.java
@@ -1,29 +1,25 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestOperations;
 
-import java.util.TimeZone;
-
-
 @SpringBootApplication
-@ComponentScan({"io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport"})
-public class App {
+@ComponentScan({ "io.pivotal.pal.tracker.timesheets", "io.pivotal.pal.tracker.restsupport" })
+@EnableEurekaClient public class App {
 
-    public static void main(String[] args) {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-        SpringApplication.run(App.class, args);
-    }
+  public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    SpringApplication.run(App.class, args);
+  }
 
-    @Bean
-    ProjectClient projectClient(
-        RestOperations restOperations,
-        @Value("${registration.server.endpoint}") String registrationEndpoint
-    ) {
-        return new ProjectClient(restOperations, registrationEndpoint);
-    }
+  @Bean ProjectClient projectClient(RestOperations restOperations,
+      @Value("${registration.server.endpoint}") String registrationEndpoint) {
+    return new ProjectClient(restOperations, registrationEndpoint);
+  }
 }
